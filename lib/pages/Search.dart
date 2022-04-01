e5824709e14fe399b088600bc3dbf728efffff2c
krishna2k3 <krishna101ravi@gmail.com>
2022-04-01 13:30:05 +0530

    create profile and search pages

--- lib/pages/Search.dart
+++ lib/pages/Search.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:cilent_hospital/pages/themes.dart';
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -11,6 +11,83 @@ class Search extends StatefulWidget {
 class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primary,
+        title: Text('Search...'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: CustomSearch());
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomSearch extends SearchDelegate {
+  List<String> hospitals = [
+    'hospital 1',
+    'hospital 2',
+    'hospital 3',
+    'hospital 4',
+  ];
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () {
+          close(context, null);
+        });
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchhQuery = [];
+    for (var item in hospitals) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchhQuery.add(item);
+      }
+    }
+    return ListView.builder(
+        itemCount: matchhQuery.length,
+        itemBuilder: (context, index) {
+          var result = matchhQuery[index];
+          return ListTile(
+            title: Text(result),
+          );
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in hospitals) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index) {
+          var result = matchQuery[index];
+          return ListTile(
+            title: Text(result),
+          );
+        });
   }
 }
